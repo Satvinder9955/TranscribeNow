84f7ff3d039613bbc647d9dd7194bf021550c4aa
Satvinder9955 <satvinder9955715289@gmail.com>
2024-10-05 04:25:41 +0530

    initial commit

---
+++ TranscribeNow-backend/Audio-Transcribe/src/test/java/com/audio/transcribe/Audio_Transcribe/AudioTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.transcribe.Audio_Transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
